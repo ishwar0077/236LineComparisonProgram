1dadcb858de0ce37425293b4c4f057a036588b8b
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-12-21 10:22:10 +0530

    Greeting Message

---
+++ LineComparison/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComparison
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Line comparison Computation Program");
+        }
+
+    }
+}
