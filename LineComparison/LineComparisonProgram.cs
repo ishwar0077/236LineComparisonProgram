5080c9c32a57dc47edf200011873d1235b8e3072
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-12-21 10:30:35 +0530

    calculating Length of line

---
+++ LineComparison/LineComparisonProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    public class LineComparisonProgram
+    {
+        public static void CalculateLine()
+        {
+            int x1 = 3;
+            int y1 = 4;
+            int x2 = 4;
+            int y2 = 3;
+            
+            double lengthOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("Lenth of Line is: " + lengthOfLine);
+        }
+    }
+}
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args) 
         {
-            Console.WriteLine("Welcome to Line comparison Computation Program");
+            LineComparisonProgram.CalculateLine();
         }
 
     }
