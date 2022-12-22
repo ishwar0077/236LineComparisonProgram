aea3376cd3eacd16d47eea5e394172a0eeb52f94
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-12-22 11:06:58 +0530

    Comparing two lines are equal, greater or less than each other

--- LineComparison/LineComparisonProgram.cs
+++ LineComparison/LineComparisonProgram.cs
@@ -11,35 +11,37 @@ namespace LineComparison
         
         public static void CalculateLine()
         {
-            Console.WriteLine("Enter x1 ");
-            double x1=Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter x2 ");
-            double x2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter y1 ");
-            double y1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter y2 ");
-            double y2 = Convert.ToDouble(Console.ReadLine());
+            double[] arr1 = new double[4];
+            for(int i = 0;i< arr1.Length;i++) 
+            {
+                Console.WriteLine("Enter value for x"+ (i + 1) +"= ");
+                arr1[i]=Convert.ToDouble(Console.ReadLine());
+            }
 
-            Console.WriteLine("Enter x3 ");
-            double x3 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter x4 ");
-            double x4 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter y3 ");
-            double y3 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter y4 ");
-            double y4 = Convert.ToDouble(Console.ReadLine());
+            double[] arr2 = new double[4];
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.WriteLine("Enter value for y" + (i + 1) + "= ");
+                arr2[i] = Convert.ToDouble(Console.ReadLine());
+            }
 
+            double x1 = arr1[0], x2 = arr1[1], x3 = arr1[2], x4 = arr1[3];
+            double y1 = arr2[0], y2 = arr2[1], y3 = arr2[2], y4 = arr2[3];
 
             double lengthOfLine1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             double lengthOfLine2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
-            bool status=lengthOfLine1.Equals(lengthOfLine2);
-            if (status)
+            int status=lengthOfLine1.CompareTo(lengthOfLine2);
+            if (status == 0)
             {
                 Console.WriteLine("{0} is equal to {1}", lengthOfLine1, lengthOfLine2);
             }
-            else
+            else if(status > 0)
             {
-                Console.WriteLine("{0} is not equal to {1}", lengthOfLine1, lengthOfLine2);
+                Console.WriteLine("{0} is greater than to {1}", lengthOfLine1, lengthOfLine2);
+            }
+            else if(status < 0)
+            {
+                Console.WriteLine("{0} is less than to {1}", lengthOfLine1, lengthOfLine2);
             }
         }
 
